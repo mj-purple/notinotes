1171cee877e2cb93d8cfc4d947e15c01be24cbd8
a201997mj <a201997mj@localhost.localdomain>
2024-04-22 17:39:59 +0200

    Basic app works

--- notinotes_app/app/src/main/java/com/example/notinotes/MainActivity.java
+++ notinotes_app/app/src/main/java/com/example/notinotes/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.notinotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inputNote;
+    private Button button;
+    private static final String NOTIFICATION_CHANNEL_ID = "0";
+    private static int NOTIFICATION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hooks
+        inputNote = findViewById(R.id.inputNote);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = inputNote.getText().toString().trim();
+                String name = String.valueOf(R.string.channel_name);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(channel);
+
+                    NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(MainActivity.this, NOTIFICATION_CHANNEL_ID)
+                            .setContentText(text)
+                            .setSmallIcon(R.drawable.baseline_edit_note_24);
+                    notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+                }
+                NOTIFICATION_ID += 1;
+                inputNote.setText("");
+            }
+        });
     }
 }
