c7304bd442b27cd153e70ae83e86a3402f53a463
a201997mj <a201997mj@localhost.localdomain>
2024-04-29 17:46:31 +0200

    add sharedpreferences

--- notinotes_app/app/src/main/java/com/example/notinotes/MainActivity.java
+++ notinotes_app/app/src/main/java/com/example/notinotes/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.core.app.NotificationCompat;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText inputNote;
     private Button button;
     private Switch persistentSwitch;
-    private static int NOTIFICATION_ID = 0;
+
+    public static final String PREF_FILE_NAME = "NotinotesPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +38,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String text = inputNote.getText().toString().trim();
-                helper.newNotification(text, NOTIFICATION_ID, persistentSwitch.isChecked());
-                NOTIFICATION_ID++;
+                helper.newNotification(text, getNextId(), persistentSwitch.isChecked());
                 inputNote.setText("");
                 finish();
             }
         });
     }
 
-
+    private int getNextId() {
+        SharedPreferences preferences = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        int nextId = preferences.getInt("notificationId", -1) + 1;
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("notificationId", nextId);
+        editor.apply();
+        return nextId;
+    }
 }
